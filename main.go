5b3523e1ecdd194a81f57903edb4a34fd99db2f9
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-08 12:31:02 -0700

    add delete movie func

--- main.go
+++ main.go
@@ -30,7 +30,15 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	fmt.Fprintln("Movie deleted successfully")
 }
 
 func main() {
