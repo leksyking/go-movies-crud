1e6dfe296dfde4f16369758e92f7fb79a747f91d
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-08 15:37:49 -0700

    fixed some bugs

--- main.go
+++ main.go
@@ -67,11 +67,14 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
 			movie.ID = item.ID
-			json.NewDecoder(r.Body).Decode(&movie)
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
 		}
 	}
-	json.NewEncoder(w).Encode(movie)
+
 }
 func main() {
 	r := mux.NewRouter()
